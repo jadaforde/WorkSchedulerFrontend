9fdbf1993bf67850a526b9d48c248f85c744881f
Joseph B <josephbettendorff@yahoo.com>
2021-11-04 10:29:46 -0500

    Bind username and password inputs to TS

--- src/app/components/loginscreen/loginscreen.component.ts
+++ src/app/components/loginscreen/loginscreen.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginscreenComponent implements OnInit {
 
+  username:string = "";
+  password:string = "";
+  
   constructor() { }
 
   ngOnInit(): void {
