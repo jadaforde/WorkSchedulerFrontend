64a5dce19b89abc67cab04818c3c828e79881298
Joseph B <josephbettendorff@yahoo.com>
2021-11-10 13:16:36 -0600

    Save user details in AppComponent so other components can use them

--- src/app/models/LoginResponse.ts
+++ src/app/models/LoginResponse.ts
@@ -1,6 +1,18 @@
-export class Employee
+import { Employee } from "src/app/models/Employee";
+
+export class LoginResponse
 {
     constructor(private employee:Employee, private isManager:boolean)
     {
+    }
+
+    public getEmployee()
+    {
+        return this.employee;
+    }
+
+    public getIsManager()
+    {
+        return this.isManager;
     }
 }
